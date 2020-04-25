5b83dad4529f31691b8ad591cab8b00759261dd3
Juan Carlos <jc.vadillo87@gmail.com>
2020-04-25 17:46:49 +0100

    Connection to DB created and Player Model

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
 
-app.use(express.json())
-app.use(router)
+//Heroku's enviorment Port
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(router);
 
 app.listen(port);
---
+++ src/config.js
@@ -0,0 +1,3 @@
+//Database connection
+//allow create variables to be able to use in other file
+module.export.connectionURL = 'mongodb+srv://test:Test@cluster0-ijah8.mongodb.net/CA2?retryWrites=true&w=majority'
