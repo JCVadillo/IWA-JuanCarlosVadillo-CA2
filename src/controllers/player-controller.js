91035230d5addd2fdb9af4f1c95665962fe98aa1
Juan Carlos <jc.vadillo87@gmail.com>
2020-04-25 21:29:31 +0100

    CRUD operations finished and tested

--- src/controllers/player-controller.js
+++ src/controllers/player-controller.js
@@ -15,6 +15,51 @@ const createPlayer = function(req, res){
   })
 }
 
+//This function retrive all players from the database.
+const getAllPlayers = function(req, res){
+  Player.find({}, function(err, players){
+    if(!players){
+      return res.status(404).send()
+    }
+    return res.send(players)
+  })
+}
+
+//Update name, club, nationality or points by of player id
+const updatePlayer = function(req, res) {
+  const _id = req.params.id
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'club', 'nationality', 'points']
+  const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+  if (!isValidUpdate){
+    return res.status(400).send({
+      error: 'Invalid update, only allowed updates: ' + allowedUpdates
+    })
+  }
+    Player.findByIdAndUpdate(_id, req.body, {new: true}).then(function(player) {
+      if (!player){
+        return res.status(404).send()
+      }
+      return res.send(player)
+    }).catch(function(error) {
+      res.status(500).send(error)
+    })
+}
+
+//Delete a player by id
+const deletePlayer = function(req, res){
+  id = req.params.id
+  Player.findOneAndRemove({ _id: id }).then(function(player){
+    res.send({player: "deleted"});
+  }).catch(function(error){
+    res.status(500).send(error)
+  })
+}
+
+
 module.exports = {
   createPlayer: createPlayer,
+  getAllPlayers: getAllPlayers,
+  updatePlayer: updatePlayer,
+  deletePlayer: deletePlayer
 };
--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,12 @@ const cors = require('cors')
 const player = require("./controllers/player-controller.js")
 
 router.all('*', cors())
-//router.<REST>('/url', <function_to_call>)
+//router.<CRUD>('/url', <function_to_call>)
 router.post('/createPlayer', player.createPlayer); //create a Player
+router.get('/getAllPlayers', player.getAllPlayers); // Get all players fromm DB
+router.patch('/player/edit/:id', player.updatePlayer); //Edit user information by id
+router.delete('/player/delete/:id', player.deletePlayer); //Delete player by id
+
 
 //If the user calls a not existing url, this error will be displayed.
 router.get('*', function(req, res) {
